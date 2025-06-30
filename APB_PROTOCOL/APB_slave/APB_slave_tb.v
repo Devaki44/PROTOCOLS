9c350bb9548aa8f40698aa1d78e0876f95406c68
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-06-30 10:37:11 +0530

    Update APB_slave_tb.v

--- APB_PROTOCOL/APB_slave/APB_slave_tb.v
+++ APB_PROTOCOL/APB_slave/APB_slave_tb.v
@@ -1,44 +1,41 @@
 module APB_slave_tb;
-    reg clk;
-	reg rst;
-	reg w_en;
-    reg [7:0]add;
-    reg [7:0]data_in;
-	wire [7:0]data_out;
-  APB_slave uut(clk,rst,w_en,add,data_in,data_out);
+  reg clk;
+  reg rst;
+  reg sel;
+  reg enable;
+  reg w_en;
+  reg [7:0]add;
+  reg [7:0]data_in;
+  wire ready;
+  wire [7:0]data_out;
+  
+  APB_slave uut(clk,rst,sel,enable,w_en,add,data_in,ready,data_out);
+  
   initial begin
-    clk = 0 ;
+    clk = 0;
     forever #5 clk = ~clk ;
   end
   initial begin
     rst=1;
-    w_en	=0;
-    add 	=3'b010;
-    data_in	=8'b11111111;
-    
-    #10;
-    rst 	=0;
-    w_en	=1;
-    add 	=3'b010;
-    data_in	=8'b11111111;
-    
-    #10;
-    rst 	=0;
-    w_en	=0;
-    data_in =0;
-    add 	=3'b010;
-    
-    #50;
-    
+    sel=0;
+    enable=0;
+    #100;
+    rst=0;
+    sel=1;
+    enable=0;
+    w_en=1;
+    add=8'hcd;
+    data_in=8'hee;
+    #1000;
+    enable=1;
+    #1000;
+    w_en=0;
+    #100_000;
     $finish;
   end
-//   initial begin
-//     $dumpfile("dump.vcd");
-//     $dumpvars;
-//   end
   initial begin
-    $monitor("$time=%0t,clk=%b,rst=%b,w_en=%b,add=%b,data_in=%b,data_out=%b",$time,clk,rst,w_en,add,data_in,data_out);
+    $dumpfile("dump.vcd");
+    $dumpvars;
   end
 endmodule
-    
     
