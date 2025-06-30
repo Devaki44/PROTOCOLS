317dbafd849b9bae8ad131815762b99370d8b2c8
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-06-30 10:36:28 +0530

    Update APB_slave.v

--- APB_PROTOCOL/APB_slave/APB_slave.v
+++ APB_PROTOCOL/APB_slave/APB_slave.v
@@ -2,17 +2,23 @@
 module APB_slave(
 	input clk,
 	input rst,
+  	input sel,
+    input enable,
 	input w_en,
 	input [7:0]add,
 	input [7:0]data_in,
+    output reg ready,
 	output reg [7:0]data_out);
 
   reg [7:0]mem[255:0];
+  
 always@(posedge clk)begin
     if(rst)begin
 		data_out <=  8'h0 ;
+      	ready    <=  0 ;
     end
-	else begin
+    else if (sel && enable)begin
+        ready <= 1 ;
 		if(w_en)
 			mem[add] <= data_in ;
 		else
@@ -20,7 +26,6 @@ always@(posedge clk)begin
 	end
 end
 endmodule
-
 
 
 
