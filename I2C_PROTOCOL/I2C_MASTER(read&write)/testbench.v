0c9e3da45f5f7f53300f2894b0c71127a976372d
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-10-17 22:14:51 +0530

    Create testbench.v

---
+++ I2C_PROTOCOL/I2C_MASTER(read&write)/testbench.v
@@ -0,0 +1,45 @@
+module testbench;
+  reg clk;
+  reg rst;
+  reg start;
+  reg w_en;
+  reg [7:0]data,data2,s_addr,s_addr2,r_addr;
+  wire sda;
+  wire temp;
+  wire sig;
+  
+  master dut(clk,rst,start,w_en,data,data2,s_addr,s_addr2,r_addr,sda,temp,sig);
+  
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk ;
+  end
+  initial begin
+    rst = 1;
+    #40;
+    rst = 0;
+    start = 1;
+    w_en = 0;
+    s_addr = 8'hcd;
+    r_addr = 8'h81;
+    s_addr2 = 8'h4e;
+    data = 8'hb7;
+    data2= 8'h6a;
+    
+   wait(!temp);
+    start = 0;
+    #20;
+    start =1;
+    #10;
+   wait(sig);
+    w_en = 1;
+  end
+  initial begin
+    #9000;
+    $finish;
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+endmodule
